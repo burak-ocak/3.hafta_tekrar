d66b00451c6a63b6ea6a14f519e9fb37de1a7081
Burak Ocak <ocakbura@gmail.com>
2023-01-30 13:52:43 +0300

    feat: Taş kağıt makas oyunu yapıldı.

---
+++ sayfa1.js
@@ -0,0 +1,34 @@
+function sonucUret(deneme){
+    let mat = Math.floor(Math.random() * 3);
+    if(mat === 0){
+        deneme.innerHTML = "Kağıt"
+    }
+    if(mat === 1){
+        deneme.innerHTML = "Makas"
+    }
+    if(mat === 2){
+        deneme.innerHTML = "Tas"
+    }
+}
+
+
+let kutu1 = document.createElement("div");
+kutu1.style.backgroundColor = "red";
+kutu1.style.height = "100px";
+document.querySelector("#app").appendChild(kutu1);
+
+let kutu2 = document.createElement("div");
+kutu2.style.backgroundColor = "blue";
+kutu2.style.height = "100px";
+kutu2.style.marginTop = "10px";
+document.querySelector("#app").appendChild(kutu2);
+
+let btn = document.createElement("button");
+btn.textContent = "Oyna";
+btn.style.marginTop = "10px";
+document.querySelector("#app").appendChild(btn);
+
+btn.addEventListener("click", function(){
+    sonucUret(kutu1)
+    sonucUret(kutu2)
+})
